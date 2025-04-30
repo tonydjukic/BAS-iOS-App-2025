6c85a0a89805cb53b4af392fe07cb5613b12f6e4
tonydjukic <tony@endlessdigital.com>
2025-04-30 19:07:45 -0400

    Initial Commit

---
+++ Brampton Adult Soccer/Brampton_Adult_SoccerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brampton_Adult_SoccerApp.swift
+//  Brampton Adult Soccer
+//
+//  Created by Tony Djukic on 2025-04-30.
+//
+
+import SwiftUI
+
+@main
+struct Brampton_Adult_SoccerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brampton Adult Soccer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Brampton Adult Soccer
+//
+//  Created by Tony Djukic on 2025-04-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
